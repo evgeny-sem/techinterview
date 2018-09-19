2d203fe9f2c35b56a1f86b944525f7738d30245b
Madhura <madhura.sambe19@gmail.com>
2018-09-18 17:38:57 -0700

    junit test for ip validation

---
+++ ipvalidation/src/main/java/com/techinterview/ipvalidation/IpValidatorTest.java
@@ -0,0 +1,16 @@
+package com.techinterview.ipvalidation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IpValidatorTest {
+
+	@Test
+	public void test() {
+//		IpValidator obj=new IpValidator();
+//		
+//		assertFalse(obj.validateIpAddress("123","234"));
+	}
+
+}
